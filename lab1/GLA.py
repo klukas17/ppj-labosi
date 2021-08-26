c94608cef4809a2c6bc1ac9df142ea7e9c7b1440
klukas17 <mihael.milicevic3@gmail.com>
2021-08-26 11:43:18 +0200

    improved input parsing in GLA

--- lab1/GLA.py
+++ lab1/GLA.py
@@ -1,7 +1,5 @@
-from enum import Enum
-
 # enumeracija za parsiranje ulazne .lan datoteke
-class Lan_parsing_step(Enum):
+class Lan_parsing_step():
 	REGULAR_DEFINITIONS = 1
 	STATES = 2
 	LEXEMES = 3
@@ -15,45 +13,38 @@ rules = []
 # generiranje LA.py datoteke
 if __name__ == "__main__":
 
-	# parsiranje cijelog ulaza sa stdin
-	lan_file = []
-	try:
-		while True:
-			lan_file.append(input())
-	except EOFError:
-		pass
-
 	# iniciranje koraka čitanja na čitanje regularnih definicija
 	current_step = Lan_parsing_step.REGULAR_DEFINITIONS
 
-	# obrada svake linije .lan datoteke
-	rules_start = None
-	for i in range(len(lan_file)):
-		line = lan_file[i]
+	# parsiranje ulaza sa stdin
+	try:
+		while True:
+			line = input()
 
-		# čitanje regularnih definicija
-		if current_step == Lan_parsing_step.REGULAR_DEFINITIONS and line[0] != "%":
-			definition = ""
-			line = line[1:]
-			while line[0] != "}":
-				definition += line[0]
+				# čitanje regularnih definicija
+			if current_step == Lan_parsing_step.REGULAR_DEFINITIONS and line[0] != "%":
+				definition = ""
 				line = line[1:]
-			regular_definitions[definition] = line[2:]
+				while line[0] != "}":
+					definition += line[0]
+					line = line[1:]
+				regular_definitions[definition] = line[2:]
 
-		# čitanje stanja
-		elif current_step == Lan_parsing_step.REGULAR_DEFINITIONS and line[0] == "%":
-			current_step = Lan_parsing_step.STATES
-			states = line[3:].split(" ")
+			# čitanje stanja
+			elif current_step <= Lan_parsing_step.STATES and line[0] == "%" and line[1] == "X":
+				current_step = Lan_parsing_step.STATES
+				states = line[3:].split(" ")
 
-		# čitanje leksičkih jedinki
-		elif current_step == Lan_parsing_step.STATES:
-			current_step = Lan_parsing_step.LEXEMES
-			lexemes = line[3:].split(" ")
+			# čitanje leksičkih jedinki
+			elif current_step <= Lan_parsing_step.LEXEMES and line[0] == "%" and line[1] == "L":
+				current_step = Lan_parsing_step.LEXEMES
+				lexemes = line[3:].split(" ")
 
-		# čitanje pravila
-		elif current_step == Lan_parsing_step.LEXEMES:
-			current_step = Lan_parsing_step.RULES
-			rules_start = i
-			break
+			# čitanje pravila
+			elif current_step <= Lan_parsing_step.RULES and line[0] != "%":
+				current_step = Lan_parsing_step.RULES
 
-	#TODO : parsiranje pravila (4. dio .lan datoteke)
+				#TODO : parsiranje pravila (4. dio .lan datoteke)
+
+	except EOFError:
+		pass
